c616bc7a8a40b744ced149598d04174dfcbaa9a1
Thien <duongthienlee@gmail.com>
2018-05-21 19:28:59 +0300

    Remove react-bootstrap, using CDN bootstrap 4 instead

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
-import FormGroup from "react-bootstrap/lib/FormGroup";
-import FormControl from "react-bootstrap/lib/FormControl";
 
 const marked = require("marked");
 
 class App extends Component {
-  state = {
-    markdown:
-      "Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](https://freecodecamp.com/hermanfassett)*"
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown:
+        "Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](https://freecodecamp.com/hermanfassett)*"
+    };
+  }
+
   updateMarkdown = event => {
     this.setState({ markdown: event.target.value });
   };
@@ -18,19 +20,23 @@ class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col">
-            <FormGroup controlId="formControlsTextarea">
-              <FormControl
-                componentClass="textarea"
-                placeholder="Enter Markdown"
-                value={this.state.markdown}
-                onChange={this.updateMarkdown}
-              >
-                {""}
-              </FormControl>
-            </FormGroup>
+            <form>
+              <div className="form-group">
+                <textarea
+                  rows="22"
+                  type="text"
+                  ref="inputValue"
+                  value={this.state.markdown}
+                  onChange={this.updateMarkdown}
+                  className="form-control"
+                />
+              </div>
+            </form>
           </div>
           <div className="col">
-            <div dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }} />
+            <div
+              dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }}
+            />
           </div>
         </div>
       </div>
